654bc8cc9417f7141df913fd8b3e204ffe45a66a
Siegfred Balona <siegblink@gmail.com>
2019-06-19 12:20:58 +0800

    Add license and contributing files

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -39,62 +39,65 @@ class Register extends Component {
 
   render() {
     return (
-      <div className="form-wrapper">
-        <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-          <main className="pa4 black-80">
-            <div className="measure">
-              <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                <legend className="f1 fw6 ph0 mh0">Register</legend>
-                <div className="mt3">
-                  <label className="db fw6 lh-copy f6" htmlFor="name">
+      <div className='form-wrapper'>
+        <article className=' center'>
+          <main className='pa4 black-80'>
+            <div className='measure'>
+              <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
+                <legend className='f1 fw6 ph0 mh0'>Register</legend>
+                <div className='mt3'>
+                  <label className='db fw6 lh-copy f6' htmlFor='name'>
                     Name
                   </label>
                   <input
                     onChange={this.onNameChange}
-                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                    type="text"
-                    name="name"
-                    id="name"
+                    className='pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
+                    type='text'
+                    name='name'
+                    id='name'
                   />
                 </div>
-                <div className="mt3">
-                  <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                <div className='mt3'>
+                  <label
+                    className='db fw6 lh-copy f6'
+                    htmlFor='email-address'
+                  >
                     Email
                   </label>
                   <input
                     onChange={this.onEmailChange}
-                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                    type="email"
-                    name="email-address"
-                    id="email-address"
+                    className='pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
+                    type='email'
+                    name='email-address'
+                    id='email-address'
                   />
                 </div>
-                <div className="mv3">
-                  <label className="db fw6 lh-copy f6" htmlFor="password">
+                <div className='mv3'>
+                  <label className='db fw6 lh-copy f6' htmlFor='password'>
                     Password
                   </label>
                   <input
                     onChange={this.onPasswordChange}
-                    className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                    type="password"
-                    name="password"
-                    id="password"
+                    className='b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
+                    type='password'
+                    name='password'
+                    id='password'
                   />
                 </div>
               </fieldset>
-              <div className="">
+              <div className=''>
                 <input
                   onClick={this.onSubmitSignIn}
-                  className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-                  type="submit"
-                  value="Register"
+                  className='b ph3 pv2 input-reset ba b--black bg-transparent pointer f6 dib'
+                  type='submit'
+                  value='Register'
                 />
               </div>
             </div>
           </main>
         </article>
       </div>
-    );
+    )
   }
 }
 
