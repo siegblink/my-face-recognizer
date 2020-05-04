d189c7deb7474425acec96775e6a647367fd258a
Sieg <siegblink@gmail.com>
2020-05-04 09:55:53 +0800

    Update navigation component styles.

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -5,21 +5,19 @@ import { Link, withRouter } from 'react-router-dom'
 import { auth } from '../../firebase/firebase-utils'
 
 const Nav = styled.nav`
-  -webkit-font-smoothing: antialiased;
-  -webkit-tap-highlight-color: transparent;
-  color: #3c3b37;
-  box-sizing: border-box;
-  background: #fff;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.08), 0 4px 12px rgba(0, 0, 0, 0.08);
   height: 4.5rem;
   padding: 0 1.6rem;
-  position: relative;
-  align-items: center;
-  display: flex;
+  font-size: 1.4rem;
   font-weight: 700;
   line-height: 1.4;
-  font-size: 1.4rem;
+  box-sizing: border-box;
+  color: #3c3b37;
+  background: #fff;
+  display: flex;
   justify-content: space-between;
+  align-items: center;
+  position: relative;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.08), 0 4px 12px rgba(0, 0, 0, 0.08);
 `
 
 const LogoText = styled.span`
@@ -28,38 +26,38 @@ const LogoText = styled.span`
 `
 
 const ButtonWrapper = styled.div`
+  padding-left: 0.8rem;
   color: #3c3b37;
+  font-size: 1.4rem;
   font-weight: 400;
   line-height: 1.4;
-  font-size: 1.4rem;
   box-sizing: border-box;
-  padding-left: 0.8rem;
   text-decoration: none;
 `
 
 const Button = styled.span`
-  box-sizing: border-box;
-  text-decoration: none;
-  position: relative;
-  align-items: center;
-  border-radius: 4px;
-  cursor: pointer;
-  display: inline-flex;
   min-width: 10rem;
-  padding: 0 1.6rem;
-  justify-content: center;
-  user-select: none;
-  vertical-align: bottom;
-  color: #0f7c90;
-  background-color: transparent;
-  ${props => props.signup && `background-color: #0f7c90; color: #d0eaef;`}
-  border: 1px solid #dcdacb;
   height: 3rem;
+  padding: 0 1.6rem;
+  font-family: inherit;
+  font-size: 1rem;
   font-weight: 700;
   line-height: 1.2;
   letter-spacing: -0.02rem;
-  font-size: 1rem;
-  font-family: inherit;
+  text-decoration: none;
+  color: #0f7c90;
+  background-color: transparent;
+  box-sizing: border-box;
+  vertical-align: bottom;
+  border: 1px solid #dcdacb;
+  border-radius: 4px;
+  cursor: pointer;
+  user-select: none;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  ${props => props.signup && `background-color: #0f7c90; color: #d0eaef;`}
 `
 
 function Navigation(props) {
